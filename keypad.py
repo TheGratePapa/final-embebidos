57920c993b71c9bff81efe9c220b235c4a1aa537
TheGratePapa <86391852+TheGratePapa@users.noreply.github.com>
2022-06-05 15:32:10 -0500

    Add files via upload

--- keypad.py
+++ keypad.py
@@ -41,13 +41,13 @@ def readLine(line, characters):
     if(GPIO.input(C1) == 1):
         #print(characters[0])
         return characters[0]
-    if(GPIO.input(C2) == 1):
+    elif(GPIO.input(C2) == 1):
         #print(characters[1])
         return characters[1]
-    if(GPIO.input(C3) == 1):
+    elif(GPIO.input(C3) == 1):
         #print(characters[2])
         return characters[2]
-    if(GPIO.input(C4) == 1):
+    elif(GPIO.input(C4) == 1):
         #print(characters[3])
         return characters[3]
     GPIO.output(line, GPIO.LOW)
