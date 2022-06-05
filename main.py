c36b8b8a6c6bc894ef87ad3532b5d5415eafe37b
TheGratePapa <86391852+TheGratePapa@users.noreply.github.com>
2022-06-05 13:21:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import keypad
+from grove_rgb_lcd import *
+
+setText("Presione D para inicializar")
+setRGB(0, 128, 15)
 
 try:
     while True:
         # call the readLine function for each row of the keypad
-        readLine(L1, ["1","2","3","A"])
-        readLine(L2, ["4","5","6","B"])
-        readLine(L3, ["7","8","9","C"])
-        readLine(L4, ["*","0","#","D"])
-        time.sleep(0.1)
+        #readLine(L1, ["1","2","3","A"])
+        #readLine(L2, ["4","5","6","B"])
+        #readLine(L3, ["7","8","9","C"])
+        trigger = readLine(L4, ["*","0","#","D"])
+        time.sleep(0.2)
+        if trigger = "D":
+            break
+
 except KeyboardInterrupt:
     print("\nApplication stopped!")
+
+setText("Digite el monto")
+setRGB(0, 128, 15)
 
