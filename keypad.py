f7a9d87f93ba170038fbfb726fb550686e9c2280
TheGratePapa <86391852+TheGratePapa@users.noreply.github.com>
2022-06-05 14:20:53 -0500

    Add files via upload

--- keypad.py
+++ keypad.py
@@ -51,5 +51,16 @@ def readLine(line, characters):
         print(characters[3])
         return characters[3]
     GPIO.output(line, GPIO.LOW)
-    return "N"
+    
+
+while true:
+    try:     
+        readLine(L1, ["1","2","3","A"])
+        readLine(L2, ["4","5","6","B"])
+        readLine(L3, ["7","8","9","C"])
+        readLine(L4, ["*","0","#","D"])
+        time.sleep(0.2)
+            
+    except KeyboardInterrupt:
+        print("\nApplication stopped!")
 
