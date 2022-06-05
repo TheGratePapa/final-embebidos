6aab6edc47b7427adc1f7b69801552d25172d7e4
TheGratePapa <a01252280@itesm.mx>
2022-06-05 12:45:34 -0500

    subida a pruebas

--- keypad.py
+++ keypad.py
@@ -48,13 +48,3 @@ def readLine(line, characters):
         print(characters[3])
     GPIO.output(line, GPIO.LOW)
 
-try:
-    while True:
-        # call the readLine function for each row of the keypad
-        readLine(L1, ["1","2","3","A"])
-        readLine(L2, ["4","5","6","B"])
-        readLine(L3, ["7","8","9","C"])
-        readLine(L4, ["*","0","#","D"])
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    print("\nApplication stopped!")
