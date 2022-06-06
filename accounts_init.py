f5663651909f618b14612fe5bbffcc29581c1625
TheGratePapa <86391852+TheGratePapa@users.noreply.github.com>
2022-06-05 23:35:24 -0500

    Add files via upload

--- accounts_init.py
+++ accounts_init.py
@@ -4,7 +4,7 @@ import sqlite3
 cards = [
     {"card_id": 0xAABBCCDD, "code": "1470", "balance": 50020},  # $500.20 = 50020
     {"card_id": 0xDDCCBBAA, "code": "0963", "balance": 1530},   # $15.30 = 1530
-    {"card_id": 2739627546, "code": "1234", "balance": 42069}   # $429.69 = 42969
+    {"card_id": 0xA34B661A, "code": "1234", "balance": 42069}   # $429.69 = 42969
 ]
 
 con = sqlite3.connect("accounts.db")
