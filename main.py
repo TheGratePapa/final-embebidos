f2017cadd2b4b8b32e8d65aac98b56999c795820
TheGratePapa <86391852+TheGratePapa@users.noreply.github.com>
2022-06-06 12:41:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,114 +4,116 @@ from Read import *
 #from buzzer import *
 from transaction import *
 
-setText("Presione D para inicializar")
-setRGB(0, 128, 15)
+while(1)
+    setText("Presione D para inicializar")
+    setRGB(0, 128, 15)
 
 
-#variables
-trigger = "N"
-charge = ""
-pin = ""
-decimal_flag = 0
+    #variables
+    trigger = "N"
+    charge = ""
+    pin = ""
+    decimal_flag = 0
 
-#salir de modo standby
-try:
-    while read_char() !="D":
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("\nApplication stopped!")
-
-#ingresar monto
-setText("Digite el monto\n >")
-setRGB(0, 128, 15)
-
-number_verification = True
-while number_verification:
+    #salir de modo standby
     try:
-       
-        while trigger != "A":
-            trigger = read_char()
-            time.sleep(0.2)
-            print("charge: " + charge)
-            print("trigger: "+ trigger)
-            if trigger.isdigit():
-                charge = charge + trigger 
-                setText("Digite el monto\n >"+str(charge))
-                setRGB(0, 128, 15)
-            elif  trigger == "*" and decimal_flag != 1:
-                charge = charge + "."
-                decimal_flag = 1
-                setText("Digite el monto\n >"+str(charge))
-                setRGB(0, 128, 15)
-
+        while read_char() !="D":
+            time.sleep(1)
     except KeyboardInterrupt:
         print("\nApplication stopped!")
-    
-    number_verification = False
-    
-    try:
-        charge = int(float(charge)*100)
-    except value_error:
-        setText("Error numerico")
-        setRGB(200, 0, 0)
-        number_verification = True
 
-#read card
-setText("coloque tarjeta")
-setRGB(50, 50, 255)
-card = card_reader()
-print("tarjeta leida: "+ str(card)) 
-#half_sec_beep()
-#password
+    #ingresar monto
+    setText("Digite el monto\n >")
+    setRGB(0, 128, 15)
 
-setText("Digite PIN\n >")
-setRGB(0, 128, 15)
-
-
-pin_verification = True
-while pin_verification:
     number_verification = True
     while number_verification:
         try:
-            n=0
-            pin = ""
-            while n<4:
+        
+            while trigger != "A":
                 trigger = read_char()
                 time.sleep(0.2)
-                print("pin: " + pin)
+                print("charge: " + charge)
                 print("trigger: "+ trigger)
                 if trigger.isdigit():
-                    pin = pin + trigger 
-                    setText("Digite el pin\n >"+str(pin))
+                    charge = charge + trigger 
+                    setText("Digite el monto\n >"+str(charge))
                     setRGB(0, 128, 15)
-                n+=1
+                elif  trigger == "*" and decimal_flag != 1:
+                    charge = charge + "."
+                    decimal_flag = 1
+                    setText("Digite el monto\n >"+str(charge))
+                    setRGB(0, 128, 15)
+
         except KeyboardInterrupt:
             print("\nApplication stopped!")
         
         number_verification = False
         
         try:
-            val = int(pin)
-            
+            charge = int(float(charge)*100)
         except value_error:
             setText("Error numerico")
             setRGB(200, 0, 0)
             number_verification = True
 
-    #Verificacion
+    #read card
+    setText("coloque tarjeta")
+    setRGB(50, 50, 255)
+    card = card_reader()
+    print("tarjeta leida: "+ str(card)) 
+    #half_sec_beep()
+    #password
 
-    setText("Verificacion")
-    setRGB(255, 255, 255)
-    if verification_check((card[0],card[1],card[2],card[3]), pin, charge)!= True:
-        print("Error en la transaccion")
-        setText("Error en la \ntransaccion")
-        setRGB(255, 30, 30)
-        time.sleep(2)
-        setText("Digite PIN\n >")
-        setRGB(0, 128, 15)
-    else:
-        print("Transaccion exitosa")
-        setText("Transaccion exitosa")
-        setRGB(30, 255, 255)
-        pin_verification = False
-        
+    setText("Digite PIN\n >")
+    setRGB(0, 128, 15)
+
+
+    pin_verification = True
+    while pin_verification:
+        number_verification = True
+        while number_verification:
+            try:
+                n=0
+                pin = ""
+                while n<4:
+                    trigger = read_char()
+                    time.sleep(0.2)
+                    print("pin: " + pin)
+                    print("trigger: "+ trigger)
+                    if trigger.isdigit():
+                        pin = pin + trigger 
+                        setText("Digite el pin\n >"+str(pin))
+                        setRGB(0, 128, 15)
+                    n+=1
+            except KeyboardInterrupt:
+                print("\nApplication stopped!")
+            
+            number_verification = False
+            
+            try:
+                val = int(pin)
+                
+            except value_error:
+                setText("Error numerico")
+                setRGB(200, 0, 0)
+                number_verification = True
+
+        #Verificacion
+
+        setText("Verificacion")
+        setRGB(255, 255, 255)
+        if verification_check((card[0],card[1],card[2],card[3]), pin, charge)!= True:
+            print("Error en la transaccion")
+            setText("Error en la \ntransaccion")
+            setRGB(255, 30, 30)
+            time.sleep(2)
+            setText("Digite PIN\n >")
+            setRGB(0, 128, 15)
+        else:
+            print("Transaccion exitosa")
+            setText("Transaccion exitosa")
+            setRGB(30, 255, 255)
+            pin_verification = False
+            time.sleep(2)
+            
